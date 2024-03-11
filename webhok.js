f27b9b808f665a2ed637286faf40103fd18d9a80
spams12 <67564865+spams12@users.noreply.github.com>
2024-03-11 15:26:08 +0300

    Update webhok.js

--- webhok.js
+++ webhok.js
@@ -39,6 +39,26 @@ app.post('/update', (req, res) => {
     io.emit('postRequestCountUpdate', postRequestCount); // Broadcast the updated post request count
     res.end('Content updated successfully');
 });
+app.get('/update', (req, res) => {
+    verifyToken = "hey";
+    console.log(req.body);
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+
+    // Check if a token and mode is in the query string of the request
+    if (mode && token) {
+        // Check the mode and token sent is correct
+        if (mode === "subscribe" && token === verifyToken) {
+            // Respond with the challenge token from the request
+            console.log("WEBHOOK_VERIFIED");
+            res.status(200).send(challenge);
+        } else {
+            // Respond with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
+});
 
 // WebSocket connection
 io.on('connection', (socket) => {
